87967f244518df173a99a427187c3a674a6a8291
Bruno <brunodp.oliveira@gmail.com>
2021-09-30 19:37:24 -0300

    Adicionando botão de terminar conversa,limpando database

--- LOCAL_DATABASE_INSTALL.md
+++ LOCAL_DATABASE_INSTALL.md
@@ -1,22 +1,20 @@
 # Instructions to install and run the databases locally correctly:
 
-- create "gala_chatbot" and "gala_feedback" blank databases in postgresql (code tested on pgAdmin4)
+- create "sarah_chatbot" blank database in postgresql (code tested on pgAdmin4)
 - Open Terminal, install guinicorn locally by typing pip install gunicorn on terminal
-- Open terminal, type "python"
 - Open IDE, open chatbot.py.
 - comment the big training list (lines 48-53)
 - decomment the "greetings.yml" training line (line 45)
 - run chatbot.py to create the database
 - go to your database, alter table properties:
 -- statement:length/precision of text,search_text,in_response_to,search_in_response_to to 1,000
--- statement:length/precision of conversation,persona to 100
--- tag: length/precision of name to 100
+-- statement:length/precision of conversation,persona to 100 
+-- tag: length/precision of name to 100 
 - decomment the big training list (lines 48-53)
 - comment the greetings training line (line 45)
 - run chatbot.py to finish the training
 - comment both training lists 
 - run/test program typing python app.py
-
 See APP_DEPLOY_HEROKU if you want to deploy this app online
 
 
--- chatbot.py
+++ chatbot.py
@@ -16,17 +16,24 @@ default_response is a response in case the bot doesn't know what to say
 maximum_similarity_threshold:
 maximum amount of similarity between 2 statement required before the search process is halted(default:0.95)
 """
-bot = ChatBot('chatbot_test',
-              read_only=True,
+bot = ChatBot('Sarah',
+              read_only=False,
               response_selection_method=get_first_response,
               logic_adapters=[{'import_path': 'chatterbot.logic.BestMatch',
                                "response_selection_method": 'chatterbot.response_selection.get_first_response',
                                "statement_comparison_function": 'chatterbot.comparisons.SynsetDistance',
                                'default_response': 'Ainda não sei responder essa frase.',
-                               'maximum_similarity_threshold': 0.90
+                               'maximum_similarity_threshold': 0.95
                                }],
               storage_adapter='chatterbot.storage.SQLStorageAdapter',
-              database_uri='postgres://ljlmrkytsrfups:bc6e9e315037054007e6a3508ee08bf92b599ba209076c4e774e487e36f653ab@ec2-52-86-25-51.compute-1.amazonaws.com:5432/da2135slnkfjp5')
+              database_uri='postgres://iuznelluxbwsrr:04344ff8343fb2d24f6b53b255898a9cb97ed35024511a8c33cf62e83962e19b@ec2-52-22-238-188.compute-1.amazonaws.com:5432/d7adm1a9fde7ib',)
+
+# local - dev config
+# database_uri='postgresql://postgres:postgres@localhost/sarah_chatbot',)
+
+# online - prod config
+# database_uri='postgres://iuznelluxbwsrr:04344ff8343fb2d24f6b53b255898a9cb97ed35024511a8c33cf62e83962e19b@ec2-52-22-238-188.compute-1.amazonaws.com:5432/d7adm1a9fde7ib',)
+
 # --------------------------------------
 
 # Corpus Trainer
@@ -34,15 +41,15 @@ corpus_trainer = ChatterBotCorpusTrainer(bot)
 
 # use this line and comment out the complete pack if
 # you feel like doing some quick testing
-# corpus_trainer.train('data/greetings.yml')
+
+corpus_trainer.train('data/greetings.yml')
 """
-corpus_trainer.train('data/botprofile.yml', 'data/compliment.yml', 'data/computers.yml', 'data/context_free_br.yml',
-                     'data/conversations.yml', 'data/emotion.yml', 'data/food.yml', 'data/games.yml',
-                     'data/gossip.yml', 'data/greetings.yml', 'data/health.yml', 'data/history.yml',
-                     'data/linguistic_knowledge.yml', 'data/literature.yml', 'data/money.yml', 'data/movies.yml',
-                     'data/politics.yml', 'data/proverbs.yml', 'data/psychology.yml', 'data/science.yml',
-                     'data/sports.yml', 'data/suggestions.yml', 'data/trivia.yml', 'data/unilab.yml')
+corpus_trainer.train('data/botprofile.yml', 'data/compliment.yml', 'data/context_free_br.yml', 'data/conversations.yml',
+                     'data/emotion.yml', 'data/food.yml', 'data/games.yml', 'data/health.yml',
+                     'data/literature.yml', 'data/money.yml', 'data/movies.yml', 'data/psychology.yml',
+                     'data/sports.yml', 'data/suggestions.yml')
 """
+
 
 # --------------------------------------
 # Conversation Loop - terminal only
@@ -54,10 +61,10 @@ def conversation():
             question = input('Usuário: ')
             answer = bot.get_response(question)
             if answer.confidence > 0.5:
-                print('bot: ', answer)
+                print('Sarah: ', answer)
                 print('confidence', answer.confidence)
             else:
-                print('bot: ', 'Ainda não sei responder essa frase')
+                print('Sarah: ', 'Ainda não sei responder essa frase')
                 print('confidence', answer.confidence)
         # CONTROL+D to exit
         except (KeyboardInterrupt, EOFError, SystemExit):
@@ -70,4 +77,4 @@ if __name__ == "__main__":
 """
 
 # --------------------------------------
-# type python chatbot.py -h localhost to train the chatbot database
+# type python chatbot.py to train the chatbot database
